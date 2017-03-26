c12197ecea9b8e74031ffa18d93ad74453bce37d
ylw12 <luwei.ying@wustl.edu>
2017-03-26 12:09:34 -0500

    Finish Prior Function
    Finish Prior Function

---
+++ easyRasch/R/Prior.R
@@ -0,0 +1,30 @@
+#' Calculate the prior
+#'
+#' Calculate the heightof the normal curve, given our believe that the prior is 
+#' always going to be a normal distribution
+#'
+#' @param theta A proposed value of theta.
+#'
+#' @return The output containing
+#'  \item{h}{The height of the specified normal curve evaluated at theta} 
+#' @author Luwei Ying
+#' @note This helps to work in a Bayesian framework
+#' @examples
+#' 
+#' theta <- 1
+#' Prior(theta)
+#' @rdname Prior
+#' @aliases Prior,ANY-method
+#' @export
+setGeneric(name="Prior",
+           def=function(theta)
+           {standardGeneric("Prior")}
+           )
+
+#' @export
+setMethod(f="Prior",
+          definition=function(theta){
+            h <- dnorm(x = theta, mean = 0, sd = 3)
+            return(h)
+          }
+          )
