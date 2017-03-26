69e8b27fb0fef73b6839b13dfa1869115779313a
ylw12 <luwei.ying@wustl.edu>
2017-03-26 15:04:10 -0500

    Add plot function
    Add plot function

---
+++ easyRasch/R/Plot.R
@@ -0,0 +1,50 @@
+#' Plot for Rasch class
+#'
+#' Take in a Rasch object and create a plot with several parameters
+#'
+#' @param raschObj An object of class Rasch
+#' @param EAP whether to have EAP added to the plot
+#'
+#' @author Luwei Ying
+#' @note This function is created to visualize the Rasch functions
+#' @examples
+#' 
+#' Trump <- new("Rasch", name = "Trump", a = -3:3, y = c(1, 1, 0, 0, 1, 0, 1))
+#' PlotRasch(Trump)
+#' @rdname PlotRasch
+#' @aliases PlotRasch,ANY-method
+#' @export
+setGeneric(name="PlotRasch",
+           def=function(raschObj, lower=-6, upper=6, EAP=TRUE)
+           {standardGeneric("PlotRasch")}
+)
+
+#' @export
+setMethod(f="PlotRasch",
+          definition=function(raschObj, lower=-6, upper=6, EAP=TRUE){
+            theta <- seq(lower, upper, by = 0.001)
+            prob <- sapply(theta, function(x) exp(x - raschObj@a)/(1 + exp(x - raschObj@a)))
+            plot(NULL,
+                 xlim = c(lower, upper),
+                 ylim = c(0, 1),
+                 xlab = "Theta",
+                 ylab = "Probability of getting the question corect",
+                 main = paste("Item Characteristic Curve for", raschObj@name))
+            sapply(1:length(raschObj@a), 
+                   function(x) lines(theta, prob[x, ], lty = 1))
+            if (EAP) {
+              abline(v = EAPRasch(raschObj, lower, upper), lty = 2)
+              legend("bottomright",
+                     legend = c("Each line for each question", "Posteriori value"),
+                     lty = c(1, 2),
+                     col = "black",
+                     cex = .5)
+            } else {
+              legend("bottomright",
+                     legend = "Each line for each question",
+                     lty = 1,
+                     col = "black",
+                     cex = .5)
+            }
+          }
+)
